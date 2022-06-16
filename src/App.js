089128be16a870ba7d0be454fc987d85c1ab05e6
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-06-15 21:59:32 -0400

    practiceTimeif returns greeting based on time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import "./styles.css";
+import "../public/styles.css";
+
+const hour = new Date();
+let time = hour.getHours();
+
+function gettime() {
+  if (time <= 12) {
+    return <h1> Good Morning </h1>;
+  } else {
+    return <h1> Good Afternoon</h1>;
+  }
+}
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  if (time <= 12) {
+    return <h1> Good Morning </h1>;
+  } else {
+    return <h1> Good Afternoon</h1>;
+  }
 }
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>good Morning, it is {time} hundred hours</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//       <p>but there's more</p>
+//     </div>
+//   );
+// }
