0d485c2fc4520805c3be757bd998933b8c451dc3
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-06-15 22:23:37 -0400

    timeIfs

--- src/App.js
+++ src/App.js
@@ -2,22 +2,37 @@ import "../public/styles.css";
 
 const hour = new Date();
 let time = hour.getHours();
+const customStyle = {
+  color: "blue"
+};
 
 function gettime() {
   if (time <= 12) {
-    return <h1> Good Morning </h1>;
+    return <h1 style={customStyle}> Good Morning </h1>;
   } else {
     return <h1> Good Afternoon</h1>;
   }
 }
 
-export default function App() {
-  if (time <= 12) {
-    return <h1> Good Morning </h1>;
-  } else {
-    return <h1> Good Afternoon</h1>;
-  }
-}
+// export default function App() {
+//   switch(time) {
+//     case 0:
+//     time <= 12 {
+//     return <h1> Good Morning </h1>;
+//   } else {
+//     return <h1> Good Afternoon</h1>;
+//   }
+// }
+
+//works for if statment
+// export default function App() {
+//   if (time <= 12) {
+//     return <h1> Good Morning </h1>;
+//   } else {
+//     return <h1> Good Afternoon</h1>;
+//   }
+// }
+
 // export default function App() {
 //   return (
 //     <div className="App">
